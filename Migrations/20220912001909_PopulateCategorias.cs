4eb9cb2adecbd40b95f8076e792071f57e8c3960
Jose Adalberto Lucas Lima <jalucas.jall@gmail.com>
2022-09-14 14:04:49 -0300

    EF Core migrations and dbcontext middleware addition to the project. Also, the database was created using SQL Server and the connection string was registered on appsettings.json

---
+++ Migrations/20220912001909_PopulateCategorias.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LanchesMC.Migrations
+{
+    public partial class PopulateCategorias : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"INSERT INTO [Categorias] (CategoriaNome, Descricao) VALUES('Normal', 'Lanche feito com ingredientes normais.')");
+            migrationBuilder.Sql(@"INSERT INTO [Categorias] (CategoriaNome, Descricao) VALUES ('Natural', 'Feito com ingredientes naturais.')");
+            migrationBuilder.Sql(@"INSERT INTO [Categorias] (CategoriaNome, Descricao) VALUES ('Artificial', 'Feito com ingredientes artificiais.')");
+            migrationBuilder.Sql(@"INSERT INTO [Categorias] (CategoriaNome, Descricao) VALUES('Angelical', 'Feito com ingredientes angelicais.')");
+            migrationBuilder.Sql(@"INSERT INTO [Categorias] (CategoriaNome, Descricao) VALUES ('Teatral', 'Feito com ingredientes teatrais.')");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM [Categorias]");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 using LanchesMC.Context;
+using LanchesMC.Repositories;
+using LanchesMC.Repositories.Interfaces;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -6,10 +8,12 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddTransient<ILancheRepository, LancheRepository>();
+builder.Services.AddTransient<ICategoriaRepository, CategoriaRepository>();
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
-
+ 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
